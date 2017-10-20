ef6c09cd7b7c1d45121860fc0d03779b01b49753
linrack <32545229+linrack@users.noreply.github.com>
2017-10-20 13:17:19 +0200

    Create Walking(Itinerary).py
    Le code doit encore être testé - Première étape

---
+++ Walking(Itinerary).py
@@ -0,0 +1,53 @@
+import Itinerary
+import requests
+
+
+class Walking(Itinerary):
+
+    URL_API_DIRECTION_Transit = 'https://maps.googleapis.com/maps/api/directions/json?&key=AIzaSyATrZmC9-XjaEAdwtPw6RG0QWV65dbywe0&mode=walking&alternatives=true'
+
+    def __init__(self, origin, destination, transit_mode, date=None, route_index=None):
+
+        Itinerary.__init__(self, origin, destination, transit_mode, date, route_index)
+        self._transit_mode = transit_mode
+
+
+        if self._transit_mode == "walking":
+
+            url_request = Transport.URL_API_DIRECTION_Transit + "&origin=" + str(self._origin) + "&destination=" + str(
+                self._destination)
+            print(url_request)
+            raw_data = requests.get(url_request).json()
+            etapes = raw_data['routes'][self.route_index]['legs'][0][
+                'steps']  # on récupère les informations concernant les différentes étapes du premier voyage proposé (celui qui correpond au plus rapide d'ailleurs...)
+
+            self._distance = etapes['distance']
+            self._duration = etapes['duration']
+            self._polyline = raw_data['routes']['overview_polyline']['points']
+
+
+        else:
+            raise TypeError("This class only takes walking as transit mode option")
+
+
+
+        @property
+        def distance(self):
+            return self._distance
+
+        @property
+        def duration(self):
+            return self._duration
+
+        @property
+        def polyline(self):
+            return self._polyline
+
+
+if __name__ == "__main__":
+    """Script de test de la bonne construction des classes"""
+
+    # Test des différentes portions d'une voyage en transport de commun
+    voyage = Walking("10 rue oswaldo cruz paris 75016", "Favella Chic Paris", "walking")
+
+
