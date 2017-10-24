99343042ecd323767aabb1065b91e7cc2ca0cc8a
M3g4lodon <mathieu.seris@student.ecp.fr>
2017-10-24 21:50:39 +0200

    Paramètres URL en constante de classe + debug d'une erreur de syntaxe de constante

--- Itinerary_Velib.py
+++ Itinerary_Velib.py
@@ -1,20 +1,22 @@
-from Itinerary import Itinerary
-from Itinerary import QueryLimit
-from Itinerary_Walking import Walking
-from Itinerary_Driving import Driving
-from Itinerary_Transit import Transit
-from Place import Place
 from datetime import timedelta
+
 import requests
+
+from Itinerary import Itinerary
+from Itinerary_Bicycling import Bicycling
+from Itinerary_Transit import Transit
+from Itinerary_Walking import Walking
+from Place import Place
 
 
 class Velib(Itinerary):
-    __URL_API_DIRECTION = 'https://maps.googleapis.com/maps/api/directions/json?&key=AIzaSyATrZmC9-XjaEAdwtPw6RG0QWV65dbywe0&mode=transit&alternatives=true'
     __URL_VELIB = 'https://opendata.paris.fr/api/records/1.0/search/'
+    __PARAMETERS = "dataset=stations-velib-disponibilites-en-temps-reel&q=status%3D%3D%22open%22+AND+available_bikes" \
+                   "%3E0 "
     __COMMUTING_DURATION = 60  # La durée de correspondance
 
     def __init__(self, origin, destination, date=None, transit_mode_type=None, itinerary_index=0):
-        
+
         Itinerary.__init__(self, origin, destination, date, transit_mode_type, itinerary_index)
         self.transport_mode = "velib"
 
@@ -22,8 +24,8 @@ class Velib(Itinerary):
         stop = True
         search_size = 1
         while stop:
-
-            parameters = "dataset=stations-velib-disponibilites-en-temps-reel&q=status%3D%3D%22open%22+AND+available_bikes%3E0&geofilter.distance=" + "%2C".join([str(origin.lat), str(origin.lng), str(search_size * 1000)])
+            parameters = Velib.__PARAMETERS + "&geofilter.distance=" + "%2C".join(
+                [str(origin.lat), str(origin.lng), str(search_size * 1000)])
             r = requests.get(Velib.__URL_VELIB, parameters)
             raw_data = r.json()
             search_size += 1
@@ -36,7 +38,8 @@ class Velib(Itinerary):
                 else:
                     stop = False
                     stations_origin.append({})
-                    stations_origin[-1]['station_address'] = Place(address=possible_station['fields']['address'])  # velib semble donner la ville et le code postal directement
+                    stations_origin[-1]['station_address'] = Place(address=possible_station['fields'][
+                        'address'])  # velib semble donner la ville et le code postal directement
                     stations_origin[-1]['nb_bike'] = available_bike
 
         fastest_path_origin = Walking(origin, stations_origin[0]['station_address'], date=self.date)
@@ -47,16 +50,15 @@ class Velib(Itinerary):
 
             transit = Transit(origin, station['station_address'], transit_mode_type="bus|rail", date=self.date)
             if transit.total_duration < fastest_path_origin.total_duration:
-            fastest_path_origin = transit
-
+                fastest_path_origin = transit
 
         # station velib à l'arrivée
         stop = True
         search_size = 1
         while stop:
-            parameters = "dataset=stations-velib-disponibilites-en-temps-reel&q=status%3D%3D%22open%22+AND+available_bikes%3E0&geofilter.distance=" \
-                         + "%2C".join([str(destination.lat), str(destination.lng), str(search_size * 1000)])
-            r = requests.get(Velib.__URL_velib, parameters)
+            parameters = Velib.__PARAMETERS + "&geofilter.distance=" + "%2C".join(
+                [str(origin.lat), str(origin.lng), str(search_size * 1000)])
+            r = requests.get(Velib.__URL_VELIB, parameters)
             raw_data = r.json()
             search_size += 1
             stations_destination = []
@@ -89,17 +91,17 @@ class Velib(Itinerary):
         velib = Bicycling(fastest_path_destination.origin, destination, date=start_date_velib)
 
         # Prise en compte du temps pour la dernière étape (station d'arrivée velib à destination)
-        start_date_last_leg = start_date_velib + timedelta(0, velib.total_duration + velib.__COMMUTING_DURATION)
+        start_date_last_leg = start_date_velib + timedelta(0, velib.total_duration + Velib.__COMMUTING_DURATION)
         if isinstance(fastest_path_destination, Walking):
-                    fastest_path_destination = Walking(origin, fastest_path_origin.destination, date=start_date_last_leg)
+            fastest_path_destination = Walking(origin, fastest_path_origin.destination, date=start_date_last_leg)
         else:
-                    fastest_path_destination = Transit(fastest_path_destination.origin, destination, date=start_date_last_leg)
+            fastest_path_destination = Transit(fastest_path_destination.origin, destination, date=start_date_last_leg)
 
         # Itineraire total = fastest_path_origin + velib + fastest_path_destination
-    
+
         self.total_duration = fastest_path_origin.total_duration \
-                             + velib.total_duration \
-                             + fastest_path_destination.total_duration
+                              + velib.total_duration \
+                              + fastest_path_destination.total_duration
         self.walking_duration = fastest_path_origin.walking_duration \
                                 + velib.walking_duration \
                                 + fastest_path_destination.walking_duration
@@ -112,15 +114,15 @@ class Velib(Itinerary):
         self.velib_duration = velib.bicycling_duration
         self.velib_distance = velib.bicycling_distance
         self.total_polyline = fastest_path_origin.total_polyline \
-                            + "\\" + velib.total_polyline \
-                            + "\\" + fastest_path_destination.total_polyline
+                              + "\\" + velib.total_polyline \
+                              + "\\" + fastest_path_destination.total_polyline
         self.information_legs = fastest_path_origin.information_legs \
                                 + velib.information_legs \
                                 + fastest_path_destination.information_legs
 
 
 if __name__ == "__main__":
-# Test des différentes portions d'une voyage en velib
+    # Test des différentes portions d'une voyage en velib
 
 
     org = Place(address="Montmartre, Paris")
@@ -128,4 +130,3 @@ if __name__ == "__main__":
     CtoD = Velib(org, des)
     print(CtoD.information_legs)
     print(repr(CtoD))
-
