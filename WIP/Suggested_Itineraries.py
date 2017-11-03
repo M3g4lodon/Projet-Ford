36db0c516b52d42357652fe090b09c552d05c709
linrack <32545229+linrack@users.noreply.github.com>
2017-11-03 12:18:55 +0100

    suggested_itineraries stocke les infos
    TODO : getters, setters et rajouter des informations (prix par ex)

--- WIP/Suggested_Itineraries.py
+++ WIP/Suggested_Itineraries.py
@@ -1,3 +1,7 @@
+#TODO setters et getters
+#TODO rajouter des info comme le prix
+#TODO faire plus de tests pour voir si tout fonctionne 
+
 from Itinerary import Itinerary
 from Itinerary_Uber import Uber
 from Itinerary_Autolib import Autolib
@@ -22,10 +26,11 @@ class Suggested_Itineraries:
         if isinstance(itinerary, Itinerary):
             self._itinerary = itinerary
         else:
-            raise TypeError("L'itinéraire doit être un objet Itinerary")
+            raise TypeError("L'origine doit être un objet Place")
 
         self._suggested_options = []
         self._suggested_itineraries = []
+        self._number_of_option = 0
 
 
 
@@ -34,33 +39,54 @@ class Suggested_Itineraries:
             #dans nos autres codes, on se limite à 3 propositions de trajet... Je prends tout ici, mettre un while <3 si besoin
             if option == 'transit':
                 transit_option = Transit(origin= self._itinerary.origin, destination = self._itinerary.destination)
-                self._suggested_options.append(transit_option)
+                self._suggested_options.append(['transit',transit_option])
             if option == 'uber':
                 uber_option = Uber(origin = self._itinerary.origin, destination = self._itinerary.destination, date=None, uber_type=None, transit_mode_type=None, itinerary_index=0)
-                self._suggested_options.append(uber_option)
+                self._suggested_options.append(['uber',uber_option])
             if option == 'bus':
                 bus_option = Transit(origin= self._itinerary.origin, destination = self._itinerary.destination, transit_mode_type='bus')
-                self._suggested_options.append(bus_option)
+                self._suggested_options.append(['bus',bus_option])
             if option == 'walking':
                 walking_option = Walking(origin= self._itinerary.origin, destination = self._itinerary.destination)
-                self._suggested_options.append(walking_option)
+                self._suggested_options.append(['walking',walking_option])
             if option == 'driving':
                 driving_option = Driving(origin= self._itinerary.origin, destination = self._itinerary.destination)
-                self._suggested_options.append(driving_option)
+                self._suggested_options.append(['driving',driving_option])
             if option == 'autolib':
                 autolib_option = Autolib(origin= self._itinerary.origin, destination = self._itinerary.destination)
-                self._suggested_options.append(autolib_option)
+                self._suggested_options.append(['autolib', autolib_option])
             if option == 'velib':
                 velib_option = Velib(origin= self._itinerary.origin, destination = self._itinerary.destination)
-                self._suggested_options.append(velib_option)
+                self._suggested_options.append(['velib',velib_option])
             if option == 'bicycling':
                 bicycling_option = Bicycling(origin= self._itinerary.origin, destination = self._itinerary.destination)
-                self._suggested_options.append(bicycling_option)
+                self._suggested_options.append(['bicycling', bicycling_option])
 
 
+        for option, transport_option_info in enumerate(self._suggested_options):
+            transport_nom = transport_option_info[0]
+            transport_option = transport_option_info[1]
+            self._suggested_itineraries.append({})
+            self._suggested_itineraries[option]['transport_type'] = transport_nom
+            self._suggested_itineraries[option]['origin']={'address': self._itinerary.origin.address, 'lat': self._itinerary.origin.lat, 'lng': self._itinerary.origin.lng}
+            self._suggested_itineraries[option]['destination'] = {'address': self._itinerary.destination.address,
+                                                             'lat': self._itinerary.destination.lat,
+                                                             'lng': self._itinerary.destination.lng}
+            self._suggested_itineraries[option]['duration'] = transport_option.total_duration
+            self._suggested_itineraries[option]['polyline'] = transit_option.total_polyline
+            self._suggested_itineraries[option]['instructions'] = repr(transport_option)
 
+            if transport_option is 'driving':
+                self._suggested_itineraries[option]['distance'] = transport_option.driving_distance
+            if transport_option is 'walking':
+                self._suggested_itineraries[option]['distance'] = transport_option.walking_distance
+            if transit_option is 'bicycling':
+                self._suggested_itineraries[option]['distance'] = transport_option.bicycling_distance
 
+            if transport_option is 'uber':
+                self._suggested_itineraries[option]['wait_time'] = transport_option.uber_wait_duration
 
+        print(self._suggested_itineraries)
 
 if __name__ == "__main__":
     """Script de test"""
