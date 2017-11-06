ff0a1690ccaa2167954cfef95703bc3b24b31fb5
barbevert <faliung@hotmail.fr>
2017-11-06 16:12:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,12 @@
 # Entry file to the server
 # To start the server, run the program, then open on your browser: http://localhost:5000/
 
-from flask import Flask, render_template, request, jsonify, Response
+from flask import Flask, render_template, request, Response
+from backend.Place import Place
+from backend.User import User
+from backend.WIP_Suggested_Itineraries import Suggested_Itineraries
+from backend.Itinerary import Itinerary
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -22,6 +27,14 @@ def itinerary_search():
     dest = request.args.get('destination',"Place de la Nation")
 
     print("type utilisateur:{}\npermis:{}\nmeteo:{}\nchargé:{}\norigine:{}\ndestination:{}\n".format(TypeUser,P_Permis,P_Meteo,P_Charge,org, dest))
+
+    origin = Place(org)
+    destination = Place(dest)
+    iti = Itinerary(origin=origin, destination=destination)
+    Utilisateur = User(TypeUser,driving_license=P_Permis, weather=P_Meteo, loaded=P_Charge)
+    Suggested_Itineraries(Utilisateur, iti)
+
+
     return Response(status=200)
 
 if __name__=='__main__':
