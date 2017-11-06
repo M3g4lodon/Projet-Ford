694804fb63b7c5e0c71c6548e6a8b0ae8c75eb94
M3g4lodon <mathieu.seris@student.ecp.fr>
2017-11-06 14:34:58 +0100

    Test Place

--- code/WIP_Test.py
+++ code/WIP_Test.py
@@ -1,17 +1,13 @@
 from unittest import TestCase
 
-from Itinerary import Itinerary
 from Place import Place
+
 
 class Test(TestCase):
     """Teste toutes les fonction du projet Ford"""
 
     def test_place(self):
-        somewhere=Place(adresse ="1 rue Joliot Curie, 91190 Gif-sur-Yvette")
-        self.assertEqual(somewhere.address,"1 rue Joliot Curie, 91190 Gif-sur-Yvette")
-        self.assertEqual(somewhere.lng,)
-        self.
-
-    def test_itinerary(self):
-        itinerary_0=Itinerary()
-        self.assertEqual()
+        somewhere = Place(address="1 rue Joliot Curie, 91190 Gif-sur-Yvette")
+        self.assertEqual(somewhere.address, "1 rue Joliot Curie, 91190 Gif-sur-Yvette")
+        self.assertEqual(somewhere.lng, 2.1632833)
+        self.assertEqual(somewhere.lat, 48.7100841)
